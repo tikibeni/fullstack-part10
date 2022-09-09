f869755b9a80a4884f9a61551c8a1694cc60f7b7
tikibeni <benjamin.blinnikka@hotmail.com>
2022-09-10 01:17:05 +0300

    Exercise 10.8.

---
+++ src/components/SignInForm.jsx
@@ -0,0 +1,17 @@
+import { Pressable, View } from "react-native";
+import FormikTextInput from "./FormikTextInput";
+import Text from "./Text";
+
+const SignInForm = ({ onSubmit }) => {
+    return (
+        <View style={{ backgroundColor: 'white', flexDirection: 'column', padding: 20 }}>
+            <FormikTextInput name="username" placeholder="Username" style={{ marginBottom: 10, borderRadius: 3, borderWidth: 1, paddingHorizontal: 15, height: 40}} />
+            <FormikTextInput name="password" placeholder="Password" secureTextEntry style={{ marginBottom: 10, borderRadius: 3, borderWidth: 1, paddingHorizontal: 15, height: 40}} />
+            <Pressable onPress={onSubmit} style={{ alignItems: 'center', backgroundColor: '#0366d6', borderRadius: 3, padding: 10 }}>
+                <Text fontWeight='bold' color='header'>Sign in</Text>
+            </Pressable>
+        </View>
+    )
+}
+
+export default SignInForm
